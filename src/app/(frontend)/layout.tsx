31b8dd4c34c5f500c80fad9b809e1c5751af0114
gzdsx <master@gzdsx.cn>
2026-05-05 17:07:03 +0800

    111

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -33,6 +33,15 @@ export default function RootLayout({
         <head>
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7306822352785197"
                     crossOrigin="anonymous"></script>
+            {`
+                <script type="text/javascript">
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "wma3jso1hz");
+                </script>
+            `}
         </head>
         <body className={`bg-black text-white min-h-screen w-full overflow-x-hidden relative overscroll-x-none`}>
         <LocaleProvider>
